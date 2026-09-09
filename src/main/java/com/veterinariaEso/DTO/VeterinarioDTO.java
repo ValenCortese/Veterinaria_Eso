5d4839464a3ded65a0f64e17200da634b010dae4
ValenCortese <valen4002@gmail.com>
2026-09-08 23:33:03 -0300

    feat: agregar Bean Validation en todos los DTOs de entrada

--- src/main/java/com/veterinariaEso/DTO/VeterinarioDTO.java
+++ src/main/java/com/veterinariaEso/DTO/VeterinarioDTO.java
@@ -3,15 +3,28 @@ package com.veterinariaEso.DTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class VeterinarioDTO {
 
+    @NotNull
+    @Min(1)
+    @Positive
     private Long id;
+
+    @NotBlank(message= "El nombre no puede estar vacío o contener solo espacios")
     private String nombre;
+
+    @NotBlank(message= "El apellido no puede estar vacío o contener solo espacios")
     private String apellido;
+
+    @NotBlank(message= "La matricula no puede estar vacía o contener solo espacios")
+    @Pattern(regexp = "^[A-Z]{2}-\\d{4}$", message = "Formato inválido. Ejemplo aceptado: MA-1234")
     private String matricula;
+
+    @NotBlank(message= "La especialidad no puede estar vacía o contener solo espacios")
     private String especialidad;
 }
